de3237dc10efc877c76b9f7afefa82b660d0e2df
Kolapo Oni <onikolapo@gmail.com>
2019-11-23 22:07:57 +0100

    create empty slider component

---
+++ src/Slider.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './slider.scss';
+
+class Slider extends Component {
+  render() {
+    return (
+      <div className="slider">
+        
+      </div>
+    );
+  }
+  
+}
+
+export default Slider;
